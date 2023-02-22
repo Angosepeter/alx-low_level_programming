ecb40e759d7b487c7699fae9ee87a79c93841768
Angosepeter <kevinangose619@gmail.com>
2023-02-21 18:33:04 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include <main.c>
 #include "holberton.h"
 /**
  * main - Entry point
@@ -10,12 +9,3 @@ int main(void)
 char str[] = "Holberton\n";
 int i = 0;
 
-while (str[i] != '\0')
-	{
-	char c = str[i];
-
-	_putchar(c);
-	i++;
-	}
-return (0);
-}
