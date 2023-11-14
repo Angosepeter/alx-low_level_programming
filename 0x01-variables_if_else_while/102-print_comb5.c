70598fa73850023e7eb259e4178a2ff41418328e
Angosepeter <kevinangose619@gmail.com>
2023-11-14 02:04:07 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**main - Entry main point.
+ * Descritption : 'program tat prints new random integer eac time it is printes'
+ * Return o - always
+ */
+
+int main() {
+    int i, j;
+
+    for (i = 0; i < 100; i++) {
+        for (j = i; j < 100; j++) {
+            putchar((i / 10) + '0');
+            putchar((i % 10) + '0');
+            putchar(' ');
+            putchar((j / 10) + '0');
+            putchar((j % 10) + '0');
+
+            if (i != 99 || j != 99) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    return 0;
+}
