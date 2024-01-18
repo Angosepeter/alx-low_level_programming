904105332d56954bc7b3fd1960a61dc33f9e6b57
Angosepeter <kevinangose619@gmail.com>
2024-01-17 23:15:31 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-
+/**
+ * main - Entry point
+ * Return:1
+ */
 int main(void)
 {
-	if (write(STDERR_FILENO, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 60) != 60)
-	{
-		return (1);
-	}
-	return (0);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
