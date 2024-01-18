fe60c6a45642ee43d92feb9041faa57f4c2db469
Angosepeter <kevinangose619@gmail.com>
2024-01-18 00:16:28 -0800

    changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
- * description - print using printf
- *
- * Return 0 - success
+ * return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
